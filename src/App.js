9e1fd17c60cd0c77013c620d01130926f6221a70
Anupriya Lathey <anupriyalathey@gmail.com>
2023-05-08 23:51:14 +0530

    added search bar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MovieList from "./components/MovieList";
 import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState(""); // empty string to start with
 
-  const getMovieRequest = async () => {
+  const getMovieRequest = async (searchValue) => {
     const url =
-      "http://www.omdbapi.com/?i=tt3896198&apikey=9af97add&s=avengers"; // hard code search(s) parameter
+      // "http://www.omdbapi.com/?i=tt3896198&apikey=9af97add&s=avengers";  hard code search(s) parameter
+      `http://www.omdbapi.com/?i=tt3896198&apikey=9af97add&s=${searchValue}`; // search(s) parameter is dynamic; quotes changed to backtick
 
     const response = await fetch(url);
     const responseJson = await response.json();
@@ -20,12 +22,13 @@ function App() {
   };
 
   useEffect(() => {
-    getMovieRequest();
-  }, []); // empty array to prevent infinite loop, request fires when the page loads
+    getMovieRequest(searchValue);
+  }, [searchValue]); // when searchValue changes getMovieRequest() is called
   return (
     <div className="container-fluid">
-      <div className="row">
+      <div className="row d-flex align-items-center mt-4 mb-4">
         <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className="row">
         <MovieList movies={movies} />
